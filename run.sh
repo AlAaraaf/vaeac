ac90d8d5b8b5d13ec5e8ced62aba78cb1041eb3f
= <=>
2023-01-26 12:11:25 -0600

    add tiny changes

--- run.sh
+++ run.sh
@@ -2,10 +2,7 @@
 cd data
 python prepare_data.py
 mkdir -p imputations
-python ../impute.py --input_file train_test_split/boston_train.tsv\ 
---output_file imputations/boston_imputed.tsv\ 
---one_hot_max_sizes 1 9 1 1 1 1 1 1 1 1 1 1 1 1\ 
---num_imputations 10 --epochs 300 --validation_ratio 0.15
+python ../impute.py --input_file train_test_split/boston_train.tsv --output_file imputations/boston_imputed.tsv --one_hot_max_sizes 1 9 1 1 1 1 1 1 1 1 1 1 1 1 --num_imputations 10 --epochs 300 --validation_ratio 0.15
 python evaluate_results.py boston 1 9 1 1 1 1 1 1 1 1 1 1 1 1
 cd ..
 echo 'finish'
