be0bef41c6dfa5a5cac9163e658ba6b17dd2b0c4
= <=>
2023-01-26 11:35:09 -0600

    add boston housing dataset

--- data/fetch_data.sh
+++ data/fetch_data.sh
@@ -5,4 +5,5 @@ cd original_data
 wget http://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data
 wget http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv
 wget http://archive.ics.uci.edu/ml/machine-learning-databases/yeast/yeast.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data
 cd ..
