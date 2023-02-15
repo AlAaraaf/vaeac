2c958deca24acf644e683dcec78fa39da5ed1357
= <=>
2023-02-15 16:34:34 -0600

    update runner version

--- run.sh
+++ run.sh
@@ -2,11 +2,20 @@
 cd data
 for i in `seq 0 9`
 do
-    python prepare_data.py $i boston 0.3 500
-    python ../impute.py --input_file train_test_split/boston_train.tsv --output_file imputations/boston_imputed.tsv --one_hot_max_sizes 1 9 1 1 1 1 1 1 1 1 1 1 1 1 --num_imputations 10 --epochs 100 --validation_ratio 0.15
-    python evaluate_results.py boston 1 9 1 1 1 1 1 1 1 1 1 1 1 1
-    python ../dataset_convert.py -dataset boston -id $i -mr 0.3 -size 500
+    python prepare_data.py $i sim_1_tiny 0.3 5000
+    
+    python ../impute.py --input_file train_test_split/sim_1_tiny_train.tsv --output_file imputations/sim_1_tiny_imputed.tsv --one_hot_max_sizes 3 2 3 4 --num_imputations 10 --epochs 100 --validation_ratio 0.15
+    python evaluate_results.py sim_1_tiny 3 2 3 4
+    
+    #python ../impute.py --input_file train_test_split/acs_train.tsv --output_file imputations/acs_imputed.tsv --one_hot_max_sizes 3 4 3 3 2 4 4 4 2 2 7 2 2 2 2 3 2 2 3 4 4 9 4 2 2 2 3 3 4 4 3 2 8 2 2 2 2 2 1 1 1 1 1 1 1 1 --num_imputations 10 --epochs 7 --validation_ratio 0.15
+    #python evaluate_results.py acs 3 4 3 3 2 4 4 4 2 2 7 2 2 2 2 3 2 2 3 4 4 9 4 2 2 2 3 3 4 4 3 2 8 2 2 2 2 2 1 1 1 1 1 1 1 1
+    
+    python ../dataset_convert.py -dataset sim_1_tiny -id $i -mr 0.3 -size 5000
 done
+
+#python ../impute.py --input_file train_test_split/boston_train.tsv --output_file imputations/boston_imputed.tsv --one_hot_max_sizes 1 9 1 1 1 1 1 1 1 1 1 1 1 1 --num_imputations 10 --epochs 100 --validation_ratio 0.15
+#python evaluate_results.py boston 1 9 1 1 1 1 1 1 1 1 1 1 1 1
+
 #python ../impute.py --input_file train_test_split/yeast_train.tsv --output_file imputations/yeast_imputed.tsv --one_hot_max_sizes 1 1 1 1 1 1 1 1 10 --num_imputations 10 --epochs 300 --validation_ratio 0.15
 #python evaluate_results.py yeast 1 1 1 1 1 1 1 1 10
 
